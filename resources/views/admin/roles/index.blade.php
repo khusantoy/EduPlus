bc3ee8a556ceb2cf362226c67e939a8382f667a4
khusanboytursunov <{119614237}+{khusanboytursunov}@users.noreply.github.com>
2023-02-07 14:10:29 +0500

    Updated Roles

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -5,11 +5,14 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
+                    <a class="btn btn-primary mb-2"  href="{{route('roles.create')}}">Create New Role</a>
                     <table class="table">
                         <thead>
                         <tr>
                             <th>Id</th>
                             <th>Role</th>
+                            <th>Permissions</th>
+                            <th>Action</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -17,6 +20,16 @@
                                 <tr>
                                     <td>{{$role->id}}</td>
                                     <td>{{$role->title}}</td>
+                                    <td></td>
+                                    <td>
+                                        <a href="{{route('roles.edit',$role->id)}}" class="btn btn-primary">Edit</a>
+                                        <a href="{{route('roles.show',$role->id)}}" class="btn btn-success">Show</a>
+                                        <form class="d-inline" action="{{route('roles.destroy',$role->id)}}" method="post">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="btn btn-danger" type="submit">Delete</button>
+                                        </form>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
